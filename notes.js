bd63f9242836fa8e93b6cd58ed70cb1a204714d6
steph medlin <stephanieanndavis13@gmail.com>
2021-11-17 19:32:46 -0700

    added notes and transfer

---
+++ notes.js
@@ -0,0 +1,26 @@
+// Datatypes = same as in Ruby
+// string "" ''
+// boolean true false
+// integers 121323 -11 0
+// floats 1.0 .32
+// hash {'boom', 'bop', 2}
+// array [1, 3, 5, 'bob']
+// nil is now called null
+// NaN
+// undefined
+
+// comments things out
+
+// variables
+// var first_name = 'bob'
+// - could contain letters, numbers, _, $ 
+// - has to begin with a letters, $, _
+// - is case sensitive
+// first_name != First_name
+// - stay away from reserved words as variable names
+// - camel case is best - firstName 
+// no longer using var because of scope 
+// use these instead: 
+// let - things you want to change
+// const - constant or things that you don't want to change
+
